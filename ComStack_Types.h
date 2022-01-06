c5b292f87bc2d6541504127de39460e73a35a7ee
Michał Szymocha <michal980731@gmail.com>
2022-01-06 12:39:08 +0100

    added Communication Stack Types

---
+++ ComStack_Types.h
@@ -0,0 +1,50 @@
+#ifndef COM_STACK_TYPES_H
+#define COM_STACK_TYPES_H
+
+/*====================================================================================================================*\
+ \@file Communication Stack Types
+
+
+\*====================================================================================================================*/
+
+#include "Std_Types.h"
+
+typedef uint16 PduIdType;
+typedef uint32 PduLengthType;
+
+typedef struct {
+    uint8 *SduDataPtr;
+    uint8 *MetaDataPtr;
+    PduLengthType SduLength;
+} PduInfoType;
+
+typedef uint8 PNCHandleType;
+
+typedef enum {
+	TP_STMIN=0x00,
+	TP_BS=0x01,
+	TP_BC=0x02
+} TPParameterType;
+
+typedef enum {
+	BUFREQ_OK=0x00,
+	BUFREQ_E_NOT_OK=0x01,
+	BUFREQ_E_BUSY=0x02,
+	BUFREQ_E_OVFL=0x03
+} BufReq_ReturnType;
+
+typedef enum {
+	TP_DATACONF=0x00,
+	TP_DATARETRY=0x01,
+	TP_CONFPENDING=0x02
+} TpDataStateType;
+
+typedef struct {
+	TpDataStateType TpDataState;
+	PduLengthType TxTpDataCnt;
+} RetryInfoType;
+
+typedef uint8 NetworkHandleType;
+
+
+#endif /* COM_STACK_TYPES_H */
