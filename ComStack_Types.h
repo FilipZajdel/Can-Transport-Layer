54b19974310041b89a7b267aab50d2d734b30288
Michał Szymocha <michal980731@gmail.com>
2022-01-26 10:34:27 +0100

    formating changes

--- ComStack_Types.h
+++ ComStack_Types.h
@@ -9,12 +9,13 @@
 
 #include "Std_Types.h"
 
-#define NULL (void*)0
+#define NULL (void *)0
 
 typedef uint16 PduIdType;
 typedef uint32 PduLengthType;
 
-typedef struct {
+typedef struct
+{
     uint8 *SduDataPtr;
     uint8 *MetaDataPtr;
     PduLengthType SduLength;
@@ -22,31 +23,34 @@ typedef struct {
 
 typedef uint8 PNCHandleType;
 
-typedef enum {
-	TP_STMIN=0x00,
-	TP_BS=0x01,
-	TP_BC=0x02
+typedef enum
+{
+    TP_STMIN = 0x00,
+    TP_BS = 0x01,
+    TP_BC = 0x02
 } TPParameterType;
 
-typedef enum {
-	BUFREQ_OK=0x00,
-	BUFREQ_E_NOT_OK=0x01,
-	BUFREQ_E_BUSY=0x02,
-	BUFREQ_E_OVFL=0x03
+typedef enum
+{
+    BUFREQ_OK = 0x00,
+    BUFREQ_E_NOT_OK = 0x01,
+    BUFREQ_E_BUSY = 0x02,
+    BUFREQ_E_OVFL = 0x03
 } BufReq_ReturnType;
 
-typedef enum {
-	TP_DATACONF=0x00,
-	TP_DATARETRY=0x01,
-	TP_CONFPENDING=0x02
+typedef enum
+{
+    TP_DATACONF = 0x00,
+    TP_DATARETRY = 0x01,
+    TP_CONFPENDING = 0x02
 } TpDataStateType;
 
-typedef struct {
-	TpDataStateType TpDataState;
-	PduLengthType TxTpDataCnt;
+typedef struct
+{
+    TpDataStateType TpDataState;
+    PduLengthType TxTpDataCnt;
 } RetryInfoType;
 
 typedef uint8 NetworkHandleType;
-
 
 #endif /* COM_STACK_TYPES_H */
--- Std_Types.h
+++ Std_Types.h
@@ -28,20 +28,20 @@ typedef uint8 Std_ReturnType;
 /* [SWS_Std_00006] */
 #ifndef STATUSTYPEDEFINED
 #define STATUSTYPEDEFINED
-#define E_OK        0x00U
+#define E_OK 0x00U
 #endif
-#define E_NOT_OK    0x01U
+#define E_NOT_OK 0x01U
 
 /* [SWS_Std_00007] */
-#define STD_HIGH     0x01U   /* Physical state 5V or 3.3V */
-#define STD_LOW      0x00U   /* Physical state 0V */
+#define STD_HIGH 0x01U /* Physical state 5V or 3.3V */
+#define STD_LOW 0x00U  /* Physical state 0V */
 
 /* [SWS_Std_00013] */
-#define STD_ACTIVE  0x01U    /* Logical state active */
-#define STD_IDLE    0x00U    /* Logical state idle */
+#define STD_ACTIVE 0x01U /* Logical state active */
+#define STD_IDLE 0x00U   /* Logical state idle */
 
 /* [SWS_Std_00010] */
-#define STD_ON      0x01U
-#define STD_OFF     0x00U
+#define STD_ON 0x01U
+#define STD_OFF 0x00U
 
 #endif /* STD_TYPES_H */
